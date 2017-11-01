63558bf7af29745cff3f289741fe3139bb81bdb3
rstatsinger <rstatsinger@gmail.com>
2017-10-31 23:57:57 -0700

    initial commit of users cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,28 @@
-#
-# Cookbook:: users
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
+
+
+# default recipe for users cookbook
+
+user 'chef' do
+	home '/home/chef'
+	password 'chef'
+end
+
+
+# template for the /etc/sshd.config file
+
+node.default['allowPasswordAuthentication'] = 'yes'
+
+template '/etc/ssh/sshd_config' do
+ 	source 'sshdConfig.erb'
+end
+
+
+service 'sshd' do
+	action :nothing
+	subscribes :restart, 'template[/etc/ssh/sshd_config]', :immediately
+end
+
+
+
+
+
--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,14 +5,26 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
+#unless os.windows?
   # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
-end
+#  describe user('root'), :skip do
+#    it { should exist }
+#  end
+#end
 
 # This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+#describe port(80), :skip do
+#  it { should_not be_listening }
+#end
+
+# user
+
+describe user('chef') do
+	it { should exist }
 end
+
+# sshd config
+ 
+describe sshd_config do
+	its('PasswordAuthentication') { should eq 'yes' }
+end 
